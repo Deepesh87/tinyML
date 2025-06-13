63094a89a9a240e4644525611712288737327e18
deepesh87 <deepesh123123@gmail.com>
2025-06-13 23:21:26 +0530

    tesor flow basics

---
+++ neural_network.py
@@ -0,0 +1,19 @@
+import numpy as np
+import tensorflow as tf
+
+# define a neural network with one neuron
+# for more information on TF functions see: https://www.tensorflow.org/api_docs
+model = tf.keras.Sequential([tf.keras.layers.Dense(units=1, input_shape=[1])])
+
+# use stochastic gradient descent for optimization and
+# the mean squared error loss function
+model.compile(optimizer='sgd', loss='mean_squared_error') #stocastic gradient descent
+
+# define some training data (xs as inputs and ys as outputs)
+xs = np.array([-1.0,  0.0, 1.0, 2.0, 3.0, 4.0], dtype=float)
+ys = np.array([-3.0, -1.0, 1.0, 3.0, 5.0, 7.0], dtype=float)
+
+# fit the model to the data (aka train the model)
+model.fit(xs, ys, epochs=500)
+
+print(model.predict(np.array([10.0])))
